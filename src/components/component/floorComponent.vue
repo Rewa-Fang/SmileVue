211d617c6150059ea0d97ac45bfa155b2fa535fa
Rewa Fang <fangrh0627@163.com>
2018-07-19 15:30:41 +0800

    floor component & moneyFilter & hotGoods & filter & van-list

---
+++ src/components/component/floorComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="floor">
+    <div class="floor-title">{{ floorTitle }}</div>
+    <div class="floor-anomaly">
+      <div class="floor1_0">
+        <img :src="floor_0.image" alt="" width="100%">
+      </div>
+      <div>
+        <div class="floor1_1"><img :src="floor_1.image" alt="" width="100%"></div>
+        <div class="floor1_2"><img :src="floor_2.image" alt="" width="100%"></div>
+      </div>
+    </div>
+    <div class="floor-rule">
+      <div v-for="(item,index) in floor.slice(3)" :key="index">
+        <img :src="item.image" alt="" width="100%">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['floor','floorTitle'],
+    data() {
+      return {
+        floor_0: {},
+        floor_1: {},
+        floor_2: {},
+      }
+    },
+    created(){
+
+    },
+    watch:{
+      floor:function(){
+        this.floor_0 = this.floor[0];
+        this.floor_1 = this.floor[1];
+        this.floor_2 = this.floor[2];
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.floor-anomaly{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd;
+  }
+  .floor-title{
+    width: 20rem;
+    height: 1.8rem;
+    line-height: 1.8rem;
+    text-align: center;
+    font-size: 14px;
+    color: #e5017d;
+  }
+  .floor-anomaly div{
+    flex: 10rem ;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+  }
+  .floor1_0{
+    border-right: 1px solid #ddd;
+  }
+  .floor1_1{
+    border-bottom: 1px solid #ddd;
+  }
+  .floor-rule{
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+  }
+  .floor-rule div{
+      width: 10rem;
+      box-sizing: border-box;
+      -webkit-box-sizing: border-box;
+  }
+  .floor-rule div:nth-child(odd){
+      border-right: 1px solid #ddd;
+  }
+</style>
--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -40,11 +40,34 @@
                     <swiper-slide v-for="(recom,index) in recommendGoods" :key="index" class="recommend-item">
                         <img v-lazy="recom.image" alt="" width="100%">
                         <div>{{ recom.goodsName }}</div>
-                        <div>￥{{ recom.price }}(￥{{ recom.mallPrice }})</div>
+                        <div class="price">￥{{ recom.price | moneyFilter }}(￥{{ recom.mallPrice | moneyFilter }})</div>
                     </swiper-slide>
                 </swiper>
             </div>
         </div>
+
+        <!-- floor1 -->
+        <floorComponent :floor="floor1" :floorTitle="floorName.floor1"></floorComponent>
+        <!-- floor2 -->
+        <floorComponent :floor="floor2" :floorTitle="floorName.floor2"></floorComponent>
+        <!-- floor3 -->
+        <floorComponent :floor="floor3" :floorTitle="floorName.floor3"></floorComponent>
+
+        <!--Hot Area-->
+        <div class="hot-area">
+            <div class="hot-title">热卖商品</div>
+            <div class="hot-goods">
+                <!--这里需要一个list组件-->
+                <van-list>
+                    <van-row>
+                        <van-col span="12" v-for="(item,index) in hotGoods" :key="index">
+                            <goodsInfo :goodsName="item.name" :goodsImage="item.image" :goodsPrice="item.price"></goodsInfo>
+                        </van-col>  
+                    </van-row>
+                </van-list>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -53,6 +76,9 @@
 import axios from 'axios'
 import 'swiper/dist/css/swiper.css'
 import {swiper,swiperSlide} from 'vue-awesome-swiper'
+import floorComponent from '../component/floorComponent'
+import goodsInfo from '../component/goodsInfoComponent'
+import { toMoney } from '@/filter/moneyFilter.js'
 export default {
     data() {
         return {
@@ -63,12 +89,23 @@ export default {
             recommendGoods :[],
             swiperOption:{
                 slidesPerView:3
-            }
+            },
+            floor1:[],
+            floor2:[],
+            floor3:[],
+            floorName:{},
+            hotGoods:[]
+        }
+    },
+    filters:{
+        moneyFilter(money){
+            return toMoney(money);
         }
     },
     components:{
         swiper,
-        swiperSlide
+        swiperSlide,
+        floorComponent,goodsInfo    
     },
     created(){
         axios({url:'https://easy-mock.com/mock/5b4ef4e52f528a212c9617d4/SmileVue/index',method:'get'})
@@ -78,6 +115,12 @@ export default {
             this.category = response.data.data.category;
             this.advertesPicture = response.data.data.advertesPicture.PICTURE_ADDRESS;
             this.recommendGoods = response.data.data.recommend;
+            this.floorName = response.data.data.floorName;
+            this.floor1 = response.data.data.floor1;
+            this.floor2 = response.data.data.floor2;
+            this.floor3 = response.data.data.floor3;
+            this.hotGoods = response.data.data.hotGoods;           //热卖商品
+
         })
         .catch(error=>{
             console.log(error);
@@ -133,25 +176,34 @@ export default {
     height: 5rem;
   } */  
   .recommend-area{
-      background-color: #fff;
-      margin-top: .3rem;
+    background-color: #fff;
+    margin-top: .3rem;
   }
   .recommend-title{
-      color: #e5017d;
-      padding: .2rem;
-      font-size: 14px;
-      border-bottom: 1px solid #eee;
+    color: #e5017d;
+    padding: .2rem;
+    font-size: 14px;
+    border-bottom: 1px solid #eee;
   }
   .recommned-body{
-      border-bottom: 1px solid #eee;
+    border-bottom: 1px solid #eee;
   }
   .recommend-item{
-      width:99%;
-      border-right: 1px solid #eee;
-      font-size: 12px;
-      text-align: center;
+    width:99%;
+    border-right: 1px solid #eee;
+    font-size: 12px;
+    text-align: center;
   }
   .recommend-item img{
-      border-bottom: 1px solid #eee;
+    border-bottom: 1px solid #eee;
+  }
+  .price{
+      color: green;
+  }
+  .hot-area{
+      text-align: center;
+      font-size:14px;
+      height: 1.8rem;
+      line-height:1.8rem;
   }
 </style>
