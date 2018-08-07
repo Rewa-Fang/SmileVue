6f82aa3e592bb1d4a5ed2bfa3f2b42a9fec9b10e
Rewa Fang <fangrh0627@163.com>
2018-08-07 22:15:36 +0800

    Main page children router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List,NavBar,Field,Cell, CellGroup,Tab,Tabs,PullRefresh,Toast,Stepper} from 'vant'
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(NavBar).use(Field).use(Cell).use(CellGroup).use(Tab).use(Tabs).use(PullRefresh).use(Toast).use(Stepper)
+import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List,NavBar,Field,Cell, CellGroup,Tab,Tabs,PullRefresh,Toast,Stepper,Tabbar, TabbarItem} from 'vant'
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload)
+.use(List).use(NavBar).use(Field).use(Cell).use(CellGroup).use(Tab).use(Tabs)
+.use(PullRefresh).use(Toast).use(Stepper).use(Tabbar).use(TabbarItem)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,27 @@ import Login from '@/components/pages/Login'
 import Goods from '@/components/pages/Goods'
 import CategoryList from '@/components/pages/CategoryList'
 import Cart from '@/components/pages/Cart'
+import Main from '@/components/pages/Main'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'ShoppingMall', component: ShoppingMall },
-    { path: '/registry', name: 'Registry', component: Registry },
-    { path: '/login', name: 'Login', component: Login },
+    {
+      path: '/main', name: 'Main', component: Main,
+      children: [
+        { path: '/', name: 'ShoppingMall', component: ShoppingMall },
+        { path: '/categoryList', name: 'CategoryList', component: CategoryList },
+        { path: '/cart', name: 'Cart', component: Cart }
+      ]
+    },
     {
       path: '/goods', name: 'Goods', component: Goods, meta: {
         keepAlive: true
       }
     },
-    { path: '/categoryList', name: 'CategoryList', component: CategoryList },
-    { path: '/cart', name: 'Cart', component: Cart }
+    { path: '/registry', name: 'Registry', component: Registry },
+    { path: '/login', name: 'Login', component: Login },
   ]
 })
